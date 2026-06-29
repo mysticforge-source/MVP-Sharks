93e0c8da13b7f2660e52000d3c9327c81bb88d8e
Riva <matvey1k@mail.ru>
2026-06-29 22:03:11 +0700

    fix: multiple screenguis instead of a folder

--- src/client/ui/AppRoot.tsx
+++ src/client/ui/AppRoot.tsx
@@ -3,11 +3,10 @@ import Container from "./components/container"
 import { Menu } from "./sources"
 import { Us2 } from "shared/utils/shortcuts"
 import Titlebutton from "./components/titlebutton"
-import text from "./text"
 import Titlescreen from "./screens/titlescreen"
 
 export = () => ( 
-    <folder>
-        <Titlescreen />
-    </folder>
+    <>
+        <Titlescreen enabled={() => Menu() === "Title"} />
+    </>
 )
