8168b0ef495b31f69a5c36dc22ac76307434c598
Riva <matvey1k@mail.ru>
2026-06-29 22:16:55 +0700

    feat: overhaul localisation

--- src/client/ui/text.ts
+++ src/client/ui/text.ts
@@ -1,10 +1,18 @@
+import { source } from "@rbxts/vide";
+
 /*
  * Collection of text used throughout the game
  */
-export = {
+export const text = {
     "en": {
         Play: "Play",
         Shop: "Shop",
         Settings: "Settings",
     }
-}
+};
+
+// localisation language source
+export const lang = source<keyof typeof text>('en');
+
+// gets localised text
+export const loc = <T extends keyof typeof text[keyof typeof text]>(prompt: T) => text[lang()][prompt]
