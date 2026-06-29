c4c68e10b8f2e53030a8127e168d127044348675
Riva <matvey1k@mail.ru>
2026-06-29 22:18:04 +0700

    chore: add nothing

--- src/client/ui/components/container.tsx
+++ src/client/ui/components/container.tsx
@@ -1,5 +1,6 @@
-import Vide from "@rbxts/vide";
-import { attr, css } from "../theme";
+import Vide from '@rbxts/vide';
+
+import { attr, css } from '../theme';
 
 export = (props: css.frame) => (
     <frame {...css.container} {...attr(props)}>
