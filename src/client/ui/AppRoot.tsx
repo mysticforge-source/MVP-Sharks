c2a93c3f1a6f4de4e5afdd01f6bbd64601a3ec6c
Riva <matvey1k@mail.ru>
2026-06-29 22:18:37 +0700

    chore: sort and clean imports

--- src/client/ui/AppRoot.tsx
+++ src/client/ui/AppRoot.tsx
@@ -1,9 +1,7 @@
-import Vide, { source, spring } from "@rbxts/vide"
-import Container from "./components/container"
-import { Menu } from "./sources"
-import { Us2 } from "shared/utils/shortcuts"
-import Titlebutton from "./components/titlebutton"
-import Titlescreen from "./screens/titlescreen"
+import Vide from '@rbxts/vide';
+
+import Titlescreen from './screens/titlescreen';
+import { Menu } from './sources';
 
 export = () => ( 
     <>
