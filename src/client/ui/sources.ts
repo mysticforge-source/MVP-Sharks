eda42b4d5e36a75e111d2f3f7be2d8c03fcf117b
Riva <matvey1k@mail.ru>
2026-07-01 13:42:37 +0700

    feat: move defaultsharkdata to shared data, add source for shark slot upgrade

--- src/client/ui/sources.ts
+++ src/client/ui/sources.ts
@@ -1,7 +1,9 @@
 // Client source of truth for UI Vide sources
 
-import { source } from "@rbxts/vide";
+import { defaultSharkSlotData } from "shared/data";
 import { SharkSlot } from "shared/networktypes";
+
+import { source } from "@rbxts/vide";
 
 // data: values
 export const coins = source<number>(0);
@@ -9,29 +11,26 @@ export const gems = source<number>(0);
 export const revivetokens = source<number>(0);
 
 // data: shark slots
-const defaultSharkData: SharkSlot = {
-	shark: 0,
-	dead: false,
-	hunger: 0,
-	exp: 0,
-	level: 0,
-};
-
-export const createSharkSource = () => source<number>(defaultSharkData.shark);
-export const createDeadSource = () => source<boolean>(defaultSharkData.dead);
-export const createHungerSource = () => source<number>(defaultSharkData.hunger);
-export const createExpSource = () => source<number>(defaultSharkData.exp);
-export const createLevelSource = () => source<number>(defaultSharkData.level);
+export const createSharkSource = () => source<number>(defaultSharkSlotData.shark);
+export const createDeadSource = () => source<boolean>(defaultSharkSlotData.dead);
+export const createHungerSource = () => source<number>(defaultSharkSlotData.hunger);
+export const createExpSource = () => source<number>(defaultSharkSlotData.exp);
+export const createUpgradeSource = () => source<number>(defaultSharkSlotData.upgrade);
+export const createLevelSource = () => source<number>(defaultSharkSlotData.level);
 
 export const createSharkSlot = () => ({
 	shark: createSharkSource(),
 	dead: createDeadSource(),
 	hunger: createHungerSource(),
 	exp: createExpSource(),
+	upgrade: createUpgradeSource(),
 	level: createLevelSource(),
 });
 
 export const slots = [createSharkSlot(), createSharkSlot(), createSharkSlot()];
+
+// Game HUD
+export const currentslot = slots[0];
 
 // UI sources
 export const Menu = source<"Title" | "Slot" | "Shop" | "Sharks" | "Settings" | "GAME">("Title");
