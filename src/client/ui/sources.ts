ce0e55a54c72a7fda9f031a8813134809ba32542
Riva <matvey1k@mail.ru>
2026-07-01 13:07:38 +0700

    feat: ui sources for the data including slots

--- src/client/ui/sources.ts
+++ src/client/ui/sources.ts
@@ -1,9 +1,37 @@
 // Client source of truth for UI Vide sources
 
 import { source } from "@rbxts/vide";
+import { SharkSlot } from "shared/networktypes";
 
+// data: values
 export const coins = source<number>(0);
+export const gems = source<number>(0);
+export const revivetokens = source<number>(0);
 
-export const Menu = source<
-    "Title" | "Slot" | "Shop" | "Sharks" | "Settings" | "GAME"
->("Title");
+// data: shark slots
+const defaultSharkData: SharkSlot = {
+	shark: 0,
+	dead: false,
+	hunger: 0,
+	exp: 0,
+	level: 0,
+};
+
+export const createSharkSource = () => source<number>(defaultSharkData.shark);
+export const createDeadSource = () => source<boolean>(defaultSharkData.dead);
+export const createHungerSource = () => source<number>(defaultSharkData.hunger);
+export const createExpSource = () => source<number>(defaultSharkData.exp);
+export const createLevelSource = () => source<number>(defaultSharkData.level);
+
+export const createSharkSlot = () => ({
+	shark: createSharkSource(),
+	dead: createDeadSource(),
+	hunger: createHungerSource(),
+	exp: createExpSource(),
+	level: createLevelSource(),
+});
+
+export const slots = [createSharkSlot(), createSharkSlot(), createSharkSlot()];
+
+// UI sources
+export const Menu = source<"Title" | "Slot" | "Shop" | "Sharks" | "Settings" | "GAME">("Title");
