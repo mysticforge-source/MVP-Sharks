379fdbb442d6c85d0cac11c6ce2203dfc35ef0af
Riva <matvey1k@mail.ru>
2026-07-03 18:02:46 +0700

    feat: (hitbox service) two new methods: clone hitbox, create player hitbox; handles hitboxes, their presence in workspace, assigns them to the player data

--- src/server/services/hitboxservice.ts
+++ src/server/services/hitboxservice.ts
@@ -1,14 +1,64 @@
 import { Service } from "@flamework/core";
-import { ReplicatedStorage } from "@rbxts/services";
+import { ReplicatedStorage, Workspace } from "@rbxts/services";
+import { DataService, PlayerToEntity } from "./dataservice";
+import { idtoshark } from "shared/data";
+import { World } from "shared/ecs/world";
+import { HitboxComponent } from "shared/ecs/components";
 
-export const PlayerToHitbox = new Map<Player, Instance>();
+//export const PlayerToHitbox = new Map<Player, Instance>();
 export const HitboxToPlayer = new Map<Instance, Player>();
 
 @Service()
 export class HitboxService {
 	// public methods
 
-	public spawnHitbox(name: string) {
-		let hitbox: Instance;
+	constructor(private readonly dataservice: DataService) {}
+
+	// clones the hitbox into workspace, does not correlate it with the player
+	public cloneHitbox(name: string): Instance | undefined {
+		let hitbox = ReplicatedStorage.Hitboxes.FindFirstChild(name);
+
+		if (hitbox && hitbox.IsA("MeshPart")) {
+			hitbox = hitbox.Clone();
+			hitbox.Parent = Workspace.Hitboxes;
+		}
+
+		return hitbox;
+	}
+
+	/**
+	 * assigns the player a hitbox
+	 * deletes old hitbox, stores the new hitbox in HitboxComponent
+	 * and HitboxToPlayer
+	 */
+	public createPlayerHitbox(player: Player, sharkname: string): Instance | undefined {
+		const data = this.dataservice.getPlayerData(player);
+
+		// getting data may fail
+		// getting shark name, id, spawning hitbox can also fail
+		if (data)
+			try {
+				const hitbox = this.cloneHitbox(sharkname);
+
+				// delete the old hitbox, get it from player data
+				const playerEntity = PlayerToEntity.get(player);
+				if (playerEntity) {
+					const prevHitbox = World.get(playerEntity, HitboxComponent);
+					if (prevHitbox) prevHitbox.Destroy();
+				}
+
+				if (hitbox) {
+					// so we could get player, having hitbox
+					// or delete it later, if none
+					HitboxToPlayer.set(hitbox, player);
+
+					// so we could get hitbox, having player
+					if (playerEntity) World.set(playerEntity, HitboxComponent, hitbox);
+
+					return hitbox;
+				}
+			} catch (e) {
+				warn(e);
+			}
 	}
 }
