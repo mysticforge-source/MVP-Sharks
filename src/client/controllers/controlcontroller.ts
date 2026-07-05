0c4f755963ca59f4b8fb9902482f502e119a4815
Riva <matvey1k@mail.ru>
2026-07-05 17:01:08 +0700

    feat: add up (E), down (Q) keybinds to inputs, bind the keybinds on begin, change the linear velocity on key presses; zerovec as an idle velocity (too insignificant to be noticed) to avoid server resetting network owner; though lacks anticheatservice and todo: make the movement relative to the camera instead

--- src/client/controllers/controlcontroller.ts
+++ src/client/controllers/controlcontroller.ts
@@ -1,28 +1,88 @@
+import { clientMaid } from "client/clientmaid";
+
 import { Controller, OnInit } from "@flamework/core";
-import { OnInput } from "./inputcontroller";
 import { StandardActionBuilder } from "@rbxts/mechanism";
 import { Players, Workspace } from "@rbxts/services";
 
+import { InputController, OnInput } from "./inputcontroller";
+
+// to avoid server reseting network owner when stopping
+const zerovec = new Vector3(0, 0.01, 0);
+
 @Controller()
 export class ControlController implements OnInput, OnInit {
-	constructor() {}
-	protected hitbox!: MeshPart;
+	constructor(private readonly inputcontroller: InputController) {}
 
+	protected hitbox!: MeshPart;
 	protected player = Players.LocalPlayer;
 
 	public alignrotation!: AlignOrientation;
+	public positionvel!: LinearVelocity;
 	public camera!: Camera;
 
-	onInit(): void | Promise<void> {
+	public movementDirection: Vector3 = zerovec;
+	protected movementVelocity: Vector3 = zerovec;
+
+	public movementSpeed = 17;
+
+	protected maid = clientMaid.sub();
+
+	onInit() {
 		this.camera = Workspace.CurrentCamera ?? (Workspace.WaitForChild("CurrentCamera")! as Camera);
 	}
 
 	public inputs = {
-		forward: new StandardActionBuilder("W"),
-		backward: new StandardActionBuilder("S"),
-		left: new StandardActionBuilder("A"),
-		right: new StandardActionBuilder("D"),
+		forward: new StandardActionBuilder("W").setProcessed(false),
+		backward: new StandardActionBuilder("S").setProcessed(false),
+		left: new StandardActionBuilder("A").setProcessed(false),
+		right: new StandardActionBuilder("D").setProcessed(false),
+		up: new StandardActionBuilder("E").setProcessed(false),
+		down: new StandardActionBuilder("Q").setProcessed(false),
 	};
+
+	// movement vectors added and removed from the movement direction
+	// upon presses of inputs
+	public movementvectors = {
+		forward: new Vector3(0, 0, -1),
+		backward: new Vector3(0, 0, 1),
+		left: new Vector3(-1, 0, 0),
+		right: new Vector3(1, 0, 0),
+		up: new Vector3(0, 1, 0),
+		down: new Vector3(0, -1, 0),
+	};
+
+	protected addDirection(direction: keyof typeof this.movementvectors) {
+		this.movementDirection = this.movementDirection.add(this.movementvectors[direction]);
+		this.directionChanged();
+	}
+
+	protected subDirection(direction: keyof typeof this.movementvectors) {
+		this.movementDirection = this.movementDirection.sub(this.movementvectors[direction]);
+		this.directionChanged();
+	}
+
+	protected directionChanged() {
+		warn("direction changed");
+
+		// normalizing to avoid diagonal speedup
+
+		let velocity = this.movementDirection;
+
+		if (velocity.Magnitude > zerovec.Magnitude) velocity = velocity.Unit;
+		else velocity = zerovec;
+
+		velocity = velocity.mul(this.movementSpeed);
+
+		this.movementVelocity = velocity; //this.hitbox.CFrame.VectorToWorldSpace(velocity);
+
+		//if (velocity.Magnitude === 0) this.movementVelocity = zerovec;
+
+		//TODO: make the movement relative to camera, by making linearvelocity
+		// relative to the world, and the force relative to the camera
+		// updated in a new system binded to tick
+
+		this.positionvel.VectorVelocity = this.movementVelocity;
+	}
 
 	// begins movement: sets hitbox, attaches camera, binds inputs
 	public begin(hitbox: MeshPart & { Attachment: Attachment }): void {
@@ -30,12 +90,18 @@ export class ControlController implements OnInput, OnInit {
 
 		this.camera.CameraSubject = this.hitbox;
 
-		// add an rotation align
-		this.alignrotation = new Instance("AlignOrientation");
-		this.alignrotation.Parent = hitbox;
-		this.alignrotation.Attachment0 = hitbox.Attachment;
-		this.alignrotation.Attachment1 = new Instance("Attachment", this.camera);
-		this.alignrotation.Responsiveness = 20;
-		this.alignrotation.Mode = Enum.OrientationAlignmentMode.OneAttachment;
+		// for the camera to rotate the hitbox
+		this.alignrotation = hitbox.WaitForChild("AlignOrientation") as AlignOrientation;
+
+		// for the inputs to move the hitbox
+		this.positionvel = hitbox.WaitForChild("LinearVelocity") as LinearVelocity;
+
+		// bind inputs
+		for (const [direction, action] of pairs(this.inputs)) {
+			this.maid.on(action.activated, () => this.addDirection(direction));
+			this.maid.on(action.deactivated, () => this.subDirection(direction));
+		}
+
+		this.inputcontroller.bindAll(this);
 	}
 }
