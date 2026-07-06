e8e661b494a3f78c519602ce2674407e22f50b92
Riva <matvey1k@mail.ru>
2026-07-06 19:58:51 +0700

    feat: update comments & update linear velocity in the system

--- src/client/systems/controlalign.ts
+++ src/client/systems/controlalign.ts
@@ -1,8 +1,18 @@
 import { ControlController } from "client/controllers/controlcontroller";
 
 // runs every frame (60hz)
+// aligns the hitbox orientation with the camera,
+//
 export = (dt: number, controlcontroller: ControlController) => {
-	if (controlcontroller.alignrotation)
-		// //
-		controlcontroller.alignrotation.CFrame = controlcontroller.camera.CFrame;
+	if (controlcontroller.alignrotation) {
+		const cameraCF = controlcontroller.camera.CFrame;
+
+		// orientation
+		// hitbox is aligned to the camera
+		controlcontroller.alignrotation.CFrame = cameraCF;
+
+		// velocity
+		// hitbox moves relative to the camera
+		controlcontroller.positionvel.VectorVelocity = cameraCF.VectorToWorldSpace(controlcontroller.movementVelocity);
+	}
 };
