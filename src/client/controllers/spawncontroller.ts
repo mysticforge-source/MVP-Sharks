b4f54a4271cb5e6d5bf9f8dd0e381ad9dfe76566
Riva <matvey1k@mail.ru>
2026-07-08 15:58:09 +0700

    feat: spawncontroller: rename Spawn, add a new method SpawnHitbox and signal HitboxAdded

--- src/client/controllers/spawncontroller.ts
+++ src/client/controllers/spawncontroller.ts
@@ -2,19 +2,25 @@ import { Controller, OnStart } from "@flamework/core";
 import { SpawnFunction } from "client/network/client";
 import { ControlController } from "./controlcontroller";
 import { Players, Workspace } from "@rbxts/services";
+import Signal from "@rbxts/lemon-signal";
+import { clientMaid } from "client/clientmaid";
 
 @Controller()
 export class SpawnController implements OnStart {
 	constructor(private readonly controlcontroller: ControlController) {}
 	protected player = Players.LocalPlayer;
+	protected maid = clientMaid.sub();
 
+	public HitboxAdded = new Signal<Model>();
+
+	/** Finds localplayer hitbox in workspace */
 	public getHitbox(): MeshPart | undefined {
 		const hitbox = Workspace.Shared.Hitboxes.FindFirstChild(this.player.Name) as MeshPart | undefined;
 		return hitbox;
 	}
 
-	/** retries multiple times to spawn the hitbox, sends a maximum of 2 calls */
-	public async Spawn(slot: number): Promise<MeshPart | undefined> {
+	/** Retries multiple times to spawn the hitbox, sends a maximum of 2 calls */
+	public async SpawnHitbox(slot: number): Promise<MeshPart | undefined> {
 		let res = SpawnFunction.call(0);
 		if (res === "Fail") {
 			// cooldown between requests
@@ -24,6 +30,14 @@ export class SpawnController implements OnStart {
 		if (res === "Fail") return;
 
 		return this.getHitbox();
+	}
+
+	/** Spawns the localplayer's hitbox and begins control */
+	public async Spawn(slot: number) {
+		const hitbox = await this.SpawnHitbox(slot);
+		if (hitbox) {
+			this.controlcontroller.begin(hitbox as any);
+		}
 	}
 
 	public onStart(): void {
@@ -37,5 +51,10 @@ export class SpawnController implements OnStart {
 
 		// it definitely is a meshpart with an attachment
 		this.controlcontroller.begin(hitbox as any);
+
+		// connect signals
+		this.maid.on(Workspace.Shared.Hitboxes.ChildAdded, (child: Model) => {
+			this.HitboxAdded.Fire(child);
+		});
 	}
 }
