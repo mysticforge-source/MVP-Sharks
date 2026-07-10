c432b4e7673072ca542307dd3737a9f8f4f4a5c7
Riva <matvey1k@mail.ru>
2026-07-10 21:15:25 +0700

    feat: viewcontroller: new type for hitbox, for shark component, replaced logic attachModel with a system, the controller attaches new entities to hitboxattached hook

--- src/client/controllers/viewcontroller.ts
+++ src/client/controllers/viewcontroller.ts
@@ -3,15 +3,39 @@ import { Players, ReplicatedStorage, Workspace } from "@rbxts/services";
 import { clientMaid } from "client/clientmaid";
 import { SpawnController } from "./spawncontroller";
 import { idtoshark } from "shared/data";
+import { World } from "shared/ecs/world";
+import { SharkViewComponent } from "client/state/components";
+import { InputController, OnInput } from "./inputcontroller";
+import { CompositeActionBuilder, StandardActionBuilder } from "@rbxts/mechanism";
+import { HitboxesVisible } from "client/state/viewstate";
+import viewlog from "client/logic/viewlog";
+
+export type Shark = {
+	sharkModel: Model;
+	hitbox: Hitbox;
+};
+
+export type Hitbox = MeshPart & {
+	ViewAttachment: Attachment;
+	SharkViewValue: IntValue;
+};
 
 @Controller()
 /**
  * Attaches models to hitboxservice's hitboxes
  */
-export class ViewController implements OnStart {
-	constructor(private readonly spawncontroller: SpawnController) {}
+export class ViewController implements OnInput, OnStart {
+	constructor(
+		private readonly spawncontroller: SpawnController,
+		private readonly inputcontroller: InputController,
+	) {}
 
+	protected viewLogic = new viewlog();
 	protected maid = clientMaid.sub();
+
+	public inputs = {
+		ToggleHitboxes: new CompositeActionBuilder("LeftControl", "H").setTiming(1),
+	};
 
 	/** Gets and clones the model from Models */
 	public cloneModel(name: string): Model | undefined {
@@ -21,33 +45,35 @@ export class ViewController implements OnStart {
 		}
 	}
 
-	/** Adds alignposition into the model, positions it in the workspace */
-	public attachModel(
-		hitbox: MeshPart & { ViewAttachment: Attachment },
-		model: Model & { Attachment: Attachment },
-	): void {
-		const attach = hitbox.ViewAttachment;
+	/**
+	 * Connected to spawncontroller.HitboxAdded
+	 * <br>Creates the shark's entity, attaching it to the view system
+	 */
+	public HitboxAttached(hitbox: Hitbox) {
+		const sharkName = idtoshark[hitbox.SharkViewValue.Value];
+		const model = this.cloneModel(sharkName) as Model & { Attachment: Attachment };
 
-		// TODO: make the model anchored,
-		// make a new interface Shark for storing a shark and its hitbox
-		// make an array, make a system that moves sharks to their hitbox cframes
-		// every render
-
-		warn("NEW MODEL");
-
-		model.PrimaryPart?.PivotTo(hitbox.GetPivot());
-		model.PrimaryPart!.CFrame = hitbox.CFrame;
 		model.Parent = Workspace.Client.Models;
+
+		// creating the shark's state immediately attaches it to viewsystem
+		const sharkEntity = World.entity();
+		World.set(sharkEntity, SharkViewComponent, {
+			sharkModel: model,
+			hitbox: hitbox,
+		});
+
+		this.maid.add(() => World.delete(sharkEntity));
 	}
 
 	public onStart(): void {
-		this.maid.on(
-			this.spawncontroller.HitboxAdded,
-			(hitbox: MeshPart & { ViewAttachment: Attachment; SharkViewValue: IntValue }) => {
-				const sharkName = idtoshark[hitbox.SharkViewValue.Value];
-				const model = this.cloneModel(sharkName) as Model & { Attachment: Attachment };
-				this.attachModel(hitbox, model);
-			},
-		);
+		this.maid.on(this.spawncontroller.HitboxAdded, (h) => this.HitboxAttached(h));
+
+		this.inputs.ToggleHitboxes.activated.Connect(() => {
+			print("111");
+			HitboxesVisible(!HitboxesVisible());
+			this.viewLogic.updateHitboxesVisibility();
+		});
+
+		this.inputcontroller.bindAll(this);
 	}
 }
