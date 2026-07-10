a2de15e4541a9945fd7af70692fc631718c5a1a2
Riva <matvey1k@mail.ru>
2026-07-10 21:10:37 +0700

    feat: new folder Logic, encapsulating logic and separating it from controllers; new viewlogic file for logic used within viewcontroller and viewsys

---
+++ src/client/logic/viewlog.ts
@@ -0,0 +1,17 @@
+import { Workspace } from "@rbxts/services";
+import { Hitbox } from "client/controllers/viewcontroller";
+import { HitboxesVisible } from "client/state/viewstate";
+
+export default class {
+	/** Moves the model to the hitbox */
+	public attachModel(hitbox: Hitbox, model: Model) {
+		model.PrimaryPart?.PivotTo(hitbox.ViewAttachment.WorldCFrame);
+	}
+
+	/** Updates visibility of all hitboxes present */
+	public updateHitboxesVisibility() {
+		for (const hitbox of Workspace.Shared.Hitboxes.GetChildren() as MeshPart[]) {
+			hitbox.Transparency = HitboxesVisible() ? 0 : 1;
+		}
+	}
+}
