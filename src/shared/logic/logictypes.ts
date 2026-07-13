f7c58a53ac7706a71e3ee5b71ebe57f1a462ed7f
Riva <matvey1k@mail.ru>
2026-07-13 22:16:36 +0700

    feat: create some type shortcuts for use in logic files

--- src/shared/logic/logictypes.ts
+++ src/shared/logic/logictypes.ts
@@ -1,5 +1,14 @@
+/**
+ * For specific response-like logic functions
+ */
 export type Result<T> = {
 	success: boolean;
 	msg?: string;
 	data: T;
 };
+
+// some type shortcuts
+export type table = {
+	[k: string]: any;
+};
+export type func<R = void> = (...args: any[]) => R;
