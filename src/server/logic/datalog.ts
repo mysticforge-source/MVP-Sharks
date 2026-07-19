9883c89b3051e193e2aa02478a88fcbedeb94004
Riva <matvey1k@mail.ru>
2026-07-19 11:46:27 +0700

    feat: figure out how to store data in logic and some more stuff about creating the collection, setting player data

--- src/server/logic/datalog.ts
+++ src/server/logic/datalog.ts
@@ -1,25 +1,78 @@
 import { Entity, World } from "@rbxts/jecs";
+import { t } from "@rbxts/t";
+import { defaultSharkSlotData } from "shared/data";
 import { func, table } from "shared/logic/logictypes";
 import { mock } from "shared/logic/mocktypes";
+import { UserData } from "shared/networktypes";
 
-export default class<DataType extends table> {
+// Data used by logic
+export const data = class {
+	datastoreName = "PROD_PlayerData";
+	defaultData = {
+		coins: 0,
+		gems: 0,
+		revivetokens: 0,
+		slots: [
+			defaultSharkSlotData,
+			defaultSharkSlotData,
+			defaultSharkSlotData,
+		],
+	};
+
+	validateSharkSlot = t.interface({
+		shark: t.numberConstrained(0, 255),
+		dead: t.boolean,
+		hunger: t.numberConstrained(0, 100),
+		exp: t.numberConstrained(0, 100),
+		upgrade: t.numberConstrained(0, 2),
+		level: t.numberConstrained(0, 255),
+	});
+
+	validateUserData = t.interface({
+		coins: t.numberConstrained(0, 100_000),
+		gems: t.numberConstrained(0, 65_535),
+		revivetokens: t.numberConstrained(0, 255),
+		slots: t.strictArray(
+			this.validateSharkSlot,
+			this.validateSharkSlot,
+			this.validateSharkSlot,
+		),
+	});
+
+	migrations: func<UserData>[] = [
+		/* v0 -> v1 */
+		(old: UserData) => {
+			old.coins = 10;
+			return old;
+		},
+	];
+};
+
+export default class {
 	constructor(
 		// Required data for logic
 		private world: World,
-		private UserDataComponent: Entity<DataType>,
+		private UserDataComponent: Entity<UserData>,
 
 		private PlayerToEntity: Map<Player, Entity>,
 		private EntityToPlayer: Map<Entity, Player>,
 
 		private PlayerDataEvent: mock.Event,
 
-		private Sessions?: Map<Player, mock.Document<DataType, true>>,
-		private Collection?: table,
+		private Lapis: mock.Lapis,
 
-		private cache = {
-			Sessions: undefined,
-		},
+		// Data created by logic
+		public Collection = this.Lapis.createCollection(
+			this.data.datastoreName,
+			{
+				defaultData: this.data.defaultData,
+				validate: this.data.validateUserData,
+				// migrations: this.data.migrations,
+			},
+		),
+		public openSessions = new Map<Player, mock.Document<UserData>>(),
 	) {}
+	private data = new data();
 
 	// PRIVATE LOGIC
 
@@ -37,21 +90,11 @@ export default class<DataType extends table> {
 	}
 
 	/** Sends the data to the player through the data sync event */
-	private fireDataEvent(player: Player, data: DataType) {
+	private fireDataEvent(player: Player, data: UserData) {
 		this.PlayerDataEvent.fire(player, data);
 	}
 
-	/** Returns the collection, errors if it wasn't created yet */
-	private getCollection() {
-		if (!this.Collection) throw error("Create the Collection first!");
-		return this.Collection;
-		// TODO: finish creating collection, also other plug in methods of dataservice
-		// fully update dataservice to only connect
-	}
-
 	// PUBLIC LOGIC
-
-	public createCollection() {}
 
 	/** Returns player's data */
 	public getPlayerData(player: Player) {
@@ -62,19 +105,37 @@ export default class<DataType extends table> {
 		}
 	}
 
-	/** Sets player's data to data */
-	public setPlayerData(player: Player, data: DataType) {
+	/** Returns player's session, if it exists */
+	public getPlayerSession(player: Player) {
+		return this.openSessions.get(player);
+	}
+
+	/** Sets player's data to data, updates the state and fires it to client */
+	public setPlayerData(player: Player, data: UserData) {
 		const entity = this.getPlayerEntity(player);
 
 		if (entity) {
 			this.world.set(entity, this.UserDataComponent, data);
+			/** Update it in the session */
+			const ses = this.getPlayerSession(player);
+			if (ses) {
+				ses.write(data);
+			} else {
+				warn("Player session does not exist");
+				// TODO: think about this and renovate dataservice
+			}
+
+			/** Fire changes to client */
+			this.fireDataEvent(player, data);
 		}
 	}
+
+	public mutatePlayerData(player: Player, data: UserData) {}
 
 	// PLUG-IN STUFF
 
 	/** Removes the player's data from maps and state */
-	public PlayerUnloaded(player: Player) {
+	public unloadPlayer(player: Player) {
 		const entity = this.getPlayerEntity(player);
 
 		if (entity) {
@@ -85,7 +146,7 @@ export default class<DataType extends table> {
 	}
 
 	/** Creates player entity, adds data, sends it to the client */
-	public PlayerLoaded(player: Player, ses: mock.Document<DataType>) {
+	public loadPlayer(player: Player, ses: mock.Document<UserData>) {
 		// maps are needed for availability of both, player and their entity
 		this.createPlayerEntity(player);
 
