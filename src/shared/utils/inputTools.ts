9dc56a8e0885767c41d751d13eaf6ffed260a568
Riva <matvey1k@mail.ru>
2026-07-23 20:45:14 +0700

    feat: replace input controller with actual input tools in shared directory

---
+++ src/shared/utils/inputTools.ts
@@ -0,0 +1,24 @@
+// Some tools regarding input
+
+import { BaseAction, InputManager } from "@rbxts/mechanism";
+
+interface inputs {
+	[key: string]: BaseAction;
+}
+
+export const inputManager = new InputManager();
+
+export const bind = inputManager.bind;
+export const unbind = inputManager.unbind;
+/** Bind all inputs */
+export const bindAll = (inputs: inputs) => {
+	for (let [_, action] of pairs(inputs)) {
+		inputManager.bind(action);
+	}
+};
+/** Unbind all inputs */
+export const unbindAll = (inputs: inputs) => {
+	for (let [_, action] of pairs(inputs)) {
+		inputManager.unbind(action);
+	}
+};
