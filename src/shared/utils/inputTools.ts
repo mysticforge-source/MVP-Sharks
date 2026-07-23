a2c5d8be50c1f53f9efbe81f84b153211e520d46
Riva <matvey1k@mail.ru>
2026-07-23 21:00:42 +0700

    feat: make inputtools export a namespace

--- src/shared/utils/inputTools.ts
+++ src/shared/utils/inputTools.ts
@@ -6,19 +6,23 @@ interface inputs {
 	[key: string]: BaseAction;
 }
 
-export const inputManager = new InputManager();
+namespace InputTools {
+	export const inputManager = new InputManager();
 
-export const bind = inputManager.bind;
-export const unbind = inputManager.unbind;
-/** Bind all inputs */
-export const bindAll = (inputs: inputs) => {
-	for (let [_, action] of pairs(inputs)) {
-		inputManager.bind(action);
-	}
-};
-/** Unbind all inputs */
-export const unbindAll = (inputs: inputs) => {
-	for (let [_, action] of pairs(inputs)) {
-		inputManager.unbind(action);
-	}
-};
+	export const bind = inputManager.bind;
+	export const unbind = inputManager.unbind;
+	/** Bind all inputs */
+	export const bindAll = (inputs: inputs) => {
+		for (let [_, action] of pairs(inputs)) {
+			inputManager.bind(action);
+		}
+	};
+	/** Unbind all inputs */
+	export const unbindAll = (inputs: inputs) => {
+		for (let [_, action] of pairs(inputs)) {
+			inputManager.unbind(action);
+		}
+	};
+}
+
+export default InputTools;
