865c2580534d44640e2da51e20c776eae73eda2b
Riva <matvey1k@mail.ru>
2026-08-04 17:17:01 +0700

    feat: aspectratio shortcut component

---
+++ src/client/ui/components/aspect.tsx
@@ -0,0 +1,7 @@
+/* aspect ratio constraint component */
+
+import Vide from "@rbxts/vide";
+
+export const Aspect = ({ aspectRatio }: { aspectRatio: number }) => (
+	<uiaspectratioconstraint AspectRatio={aspectRatio} />
+);
