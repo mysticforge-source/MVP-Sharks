40525c211fae695a87e35d782209b1fcaab3c056
Riva <matvey1k@mail.ru>
2026-08-08 21:47:50 +0700

    feat(spawncontroller): deprecate useless methods, use remote events

--- src/client/controllers/SpawnController.ts
+++ src/client/controllers/SpawnController.ts
@@ -1,9 +1,10 @@
 import { Controller, OnStart } from "@flamework/core";
-import { SpawnFunction } from "client/network/client";
+import { SpawnResult, SpawnSlot } from "client/network/client";
 import { MovementController } from "./MovementController";
 import { Players, Workspace } from "@rbxts/services";
 import Signal from "@rbxts/lemon-signal";
 import { clientMaid } from "client/clientmaid";
+import { Menu } from "client/ui/sources";
 
 @Controller()
 /*
@@ -30,25 +31,9 @@ export class SpawnController implements OnStart {
 		return hitbox;
 	}
 
-	/* retries multiple times to spawn the hitbox */
-	public async SpawnHitbox(slot: number): Promise<MeshPart | undefined> {
-		let res = SpawnFunction.call(0);
-		if (res === "Fail") {
-			// cooldown between requests
-			task.wait(0.5);
-			res = SpawnFunction.call(0);
-		}
-		if (res === "Fail") return;
-
-		return this.getHitbox();
-	}
-
 	/* spawns the localplayer's hitbox and begins control */
 	public async Spawn(slot: number) {
-		const hitbox = await this.SpawnHitbox(slot);
-		if (hitbox) {
-			this.MovementController.begin(hitbox as any);
-		}
+		SpawnSlot.fire(slot);
 	}
 
 	public onStart(): void {
@@ -56,19 +41,23 @@ export class SpawnController implements OnStart {
 		const authorityFolder = this.getAuthorityFolder();
 		this.maid.on(authorityFolder.ChildAdded, (child: Model) => {
 			warn("[SpawnController] CHILD ADDED to ServerAuthority");
+			child.WaitForChild("SharkViewValue");
 			this.HitboxAdded.Fire(child);
 		});
 
-		task.wait(5);
-		// for testing
-		const res = SpawnFunction.call(0);
-		warn(`[SpawnController] Spawn result: ${res}`);
-
-		const hitbox = this.getHitbox();
-		warn(hitbox);
-
-		if (hitbox) {
-			this.MovementController.begin(hitbox as any);
-		}
+		this.maid.add(
+			SpawnResult.on(() => {
+				let hitbox = this.getHitbox();
+				if (!hitbox) {
+					task.wait(1);
+					hitbox = this.getHitbox();
+				}
+				if (hitbox) {
+					task.wait();
+					this.MovementController.begin(hitbox as any);
+					Menu("GAME");
+				}
+			}),
+		);
 	}
 }
